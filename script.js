78220fd3ac57d17901e41372ead023c8eb2a49ae
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-26 12:09:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function insert_Row() {
     //Write your code here
   let table= document.getElementById('sampleTable');
-	let btn = document.querySelector('input');
+	// let btn = document.querySelector('input');
 	
-  btn.addEventListener('click', addRow )
-	function addRow(){
+ //  btn.addEventListener('click', addRow )
+	// function addRow(){
 		let row = table.insertRow(0);
 		row.insertCell(0).innerHTML = "New Cell1";
 		row.insertCell(1).innerHTML = "New Cell2";
 
-}
+// }
 }
